1d25d273dff12d99595760379c175d9386efd28f
ZPLOVEZYH <1031626336@qq.com>
2018-12-12 10:58:48 +0800

    1、添加到github上

--- src/main/java/com/yuanjun/mybatis/controller/IbkController.java
+++ src/main/java/com/yuanjun/mybatis/controller/IbkController.java
@@ -1,16 +1,26 @@
 package com.yuanjun.mybatis.controller;
 
 import com.yuanjun.mybatis.entity.RestResult;
+import com.yuanjun.mybatis.entity.ResultBean;
 import com.yuanjun.mybatis.service.IbkService;
+import com.yuanjun.mybatis.util.ExcelUtils;
 import com.yuanjun.mybatis.util.ResultGenerator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping("ibk")
@@ -49,4 +59,78 @@ public class IbkController {
 
         return modelAndView;
     }
+
+    @ResponseBody
+    @RequestMapping("/view")
+    public ModelAndView view() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+
+        return modelAndView;
+    }
+
+    @ResponseBody
+    @RequestMapping("/excel/{registNum}")
+    public void excel(@PathVariable int registNum) {
+        RestResult restResult = null;
+        try {
+            restResult = ibkService.requestExportExcel(registNum);
+        } catch (IOException e) {
+            restResult = resultGenerator.getFailResult("excel导出异常" + e.getMessage());
+        }
+
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        HttpServletResponse response = servletRequestAttributes.getResponse();
+
+        // excel标题
+        String[] title = {"运单号"};
+
+        // excel文件名
+        String fileName = "运单号信息表" + System.currentTimeMillis() + ".xls";
+
+        //sheet名
+        String sheetName = "运单号信息表";
+        List<ResultBean> list = (List<ResultBean>) restResult.getData();
+        String[][] content = new String[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            content[i] = new String[title.length];
+            ResultBean obj = list.get(i);
+            content[i][0] = obj.getTransNum();
+        }
+
+        //创建HSSFWorkbook
+        HSSFWorkbook wb = ExcelUtils.getHSSFWorkbook(sheetName, title, content, null);
+
+        //响应到客户端
+        try {
+            this.setResponseHeader(response, fileName);
+            OutputStream os = response.getOutputStream();
+            wb.write(os);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //发送响应流方法
+    public void setResponseHeader(HttpServletResponse response, String fileName) {
+        try {
+            try {
+                fileName = new String(fileName.getBytes(), "ISO8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            response.setContentType("application/octet-stream;charset=ISO8859-1");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.addHeader("Pargam", "no-cache");
+            response.addHeader("Cache-Control", "no-cache");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
 }
---
+++ src/main/resources/WEB-INF/views/index.ftl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<script>
+    function displayDate() {
+        window.location.href = "/ibk/excel/2";
+    }
+</script>
+<body>
+<button id="js-export" type="button" onclick="displayDate()" class="btn btn-primary">导出Excel</button>
+</body>
+</html>
